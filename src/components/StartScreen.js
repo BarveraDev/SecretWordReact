12598a2dea3fb3c4759d055332e977833613401b
Luiz Paulo <lpbalves7filho@gmail.com>
2023-02-22 12:55:41 -0300

    React secret word

---
+++ src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Click on the button below for play the game</p>
+      <button>Start the game</button>
+    </div>
+  );
+};
+
+export default StartScreen;
---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordslist = {
+  carro: ["Motor","Porta", "Capô", "Pneu", "Antena", "Rádio", "Retrovisor"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Goiaba"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Mão"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Headset"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "NodeJs"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Frango"]
+}
